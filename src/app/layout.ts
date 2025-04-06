5e945c6b031afe00daba64269a6a683d3f8121ef
justin <j.anhalt@live.com>
2025-04-06 17:40:40 -0400

    Fix layout metadata export issue and separate into layout.ts

---
+++ src/app/layout.ts
@@ -0,0 +1,7 @@
+// src/app/layout.ts
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Titan Defense Technologies",
+  description: "Canadian defense innovation at its finest.",
+};
